4fd58b1a5a75d19b8a47897f8ca028158ec9e9cf
Kevin <kevin@kehei.de>
2019-04-17 22:09:32 +0200

    Babelify PersonEditForm

--- database/forms.py
+++ database/forms.py
@@ -1,17 +1,18 @@
 from flask_wtf import FlaskForm
+from flask_babel import _
 from wtforms import StringField, SubmitField, DateField, BooleanField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Email, Optional
 
 
 class PersonEditForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    email = EmailField('E-Mail-Adresse', validators=[DataRequired(), Email()])
-    date_of_birth = DateField('Geburtstag',
+    name = StringField(_('Name'), validators=[DataRequired()])
+    email = EmailField(_('E-Mail-Adresse'), validators=[DataRequired(), Email()])
+    date_of_birth = DateField(_('Geburtstag'),
                               validators=[Optional()],
-                              format='%d.%m.%Y')
-    joining_date = DateField('Mitglied seit', render_kw={'readonly': True})
-    membership_pending = BooleanField('Mitgliedschaft beantragt')
-    member = BooleanField('Mitgliedschaft bestätigt',
+                              format=_('%d.%m.%Y'))
+    joining_date = DateField(_('Mitglied seit'), render_kw={'readonly': True})
+    membership_pending = BooleanField(_('Mitgliedschaft beantragt'))
+    member = BooleanField(_('Mitgliedschaft bestätigt'),
                           render_kw={'readonly': True})
-    submit = SubmitField('Speichern')
+    submit = SubmitField(_('Speichern'))
