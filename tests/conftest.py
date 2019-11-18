04eb8abda07679ceaf2a5e5dabd599d2f2d282fa
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2019-11-18 10:41:33 +0100

    Reduce to the necessary

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,44 +1,20 @@
 import pytest
+import tempfile
+
+
+
+
 
 @pytest.fixture(scope='module')
-def new_user():
-    from database.models import Person, User
+def client():
+    from database import app, db
 
-    person = Person(name='Alfred Nobel', email='alfred@tu-dortmund.de')
-    user = User(person=person, username='anobel')
-    user.set_password('testpw')
-    return user
+    with tempfile.NamedTemporaryFile(suffix='.sqlite', prefix='db_testing') as f:
+        app.config.from_object('config.TestingConfig')
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + f.name
 
-@pytest.fixture(scope='module')
-def test_client():
-    from database import app
 
-    app.config.from_object('config.TestingConfig')
-
-    testing_client = app.test_client()
-
-    ctx = app.app_context()
-    ctx.push()
-
-    yield testing_client
-
-    ctx.pop()
-
-#@pytest.fixture(scope='module')
-#def init_database():
-#    db.create_all()
-#
-#    # Insert user data
-#    person1 = Person(name='Alfred Nobel', email='alfred@tu-dortmund.de')
-#    user1 = User(person=person1, username='anobel')
-#    person2 = Person(name='Nikola Tesla', email='nikola@tu-dortmund.de')
-#    user2 = User(person=person2, username='ntesla')
-#    db.session.add(user1)
-#    db.session.add(user2)
-#
-#    # Commit the changes for the users
-#    db.session.commit()
-#
-#    yield db  # this is where the testing happens!
-#
-#    db.drop_all()
+        with app.test_client() as client:
+            with app.app_context():
+                db.create_all()
+                yield client
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,17 +1,18 @@
-def test_home(test_client):
+def test_home(client):
     """
     GIVEN a Flask application
     WHEN the '/' page is requested (GET)
     THEN check the response is valid
     """
-    response = test_client.get('/')
+    response = client.get('/')
     assert response.status_code == 200
 
-def test_login(test_client):
+
+def test_login(client):
     """
     GIVEN a Flask application
     WHEN the '/login' page is requested (GET)
     THEN check the response is valid
     """
-    response = test_client.get('/login')
+    response = client.get('/login')
     assert response.status_code == 200
