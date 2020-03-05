d28697929589ead9c311e9ea69cfd682786a7e67
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2020-03-05 13:12:52 +0100

    Fix strange email behaviour in tests

--- member_database/mail.py
+++ member_database/mail.py
@@ -12,29 +12,37 @@ socket.setdefaulttimeout(30)
 mail = Mail()
 
 
-def on_backoff(details):
-    log.error('Sending email failed in {tries} attempt, waiting {wait:.1f} s.')
-
-
-@backoff.on_exception(
-    backoff.expo,
-    Exception,
-    max_tries=18,
-    on_backoff=on_backoff,
-)
-def send_mail(app, msg):
+def send_msg_async(msg):
     '''
-    Function to be called from a Thread to send msg in backoground.
+    Calls a mail.send(msg) in a background thread.
+    Retries on errors using exponential backoff.
     See https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-x-email-support
     '''
-    log.info(f'Sending mail with subject "{msg.subject}" to {msg.recipients}')
-    try:
+    def on_backoff(details):
+        log.error('Sending email failed in {tries} attempt, waiting {wait:.1f} s.')
+
+    @backoff.on_exception(
+        backoff.expo,
+        Exception,
+        max_tries=18,
+        on_backoff=on_backoff,
+    )
+    def target(app, mail):
+        log.info(f'Sending mail with subject "{msg.subject}" to {msg.recipients}')
         with app.app_context():
-            mail.send(msg)
-    except Exception:
-        logging.exception('Failed sending mail')
-        raise
-    log.info('Mail sent')
+            try:
+                mail.send(msg)
+            except Exception:
+                logging.exception('Failed sending mail')
+                raise
+        log.info('Mail sent')
+
+    # See https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-xv-a-better-application-structure
+    # For an explanation of the current_app magic
+    Thread(
+        target=target,
+        args=(current_app._get_current_object(), msg)
+    ).start()
 
 
 def send_email(subject, sender, recipients, body, **kwargs):
@@ -47,13 +55,8 @@ def send_email(subject, sender, recipients, body, **kwargs):
     # capturing mails does not work in another thread
     # so just send it here for the unit tests
     if current_app.config['TESTING']:
-        send_mail(current_app._get_current_object(), msg)
+        mail.send(msg)
     elif current_app.config['DEBUG'] is True:
         print(body)
     else:
-        # See https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-xv-a-better-application-structure
-        # For an explanation of the current_app magic
-        Thread(
-            target=send_email,
-            args=(current_app._get_current_object(), msg)
-        ).start()
+        send_msg_async(msg)
