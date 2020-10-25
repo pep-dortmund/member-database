7d83daf3c016f8d726cb1aa1df79cd527a086835
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2020-10-25 20:47:07 +0100

    Add naming conventions for indices

--- member_database/models/base.py
+++ member_database/models/base.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 import json
 import decimal
 
@@ -19,7 +20,18 @@ def dumps(*args, **kwargs):
     return json.dumps(*args, **kwargs)
 
 
-db = SQLAlchemy(engine_options={'json_serializer': dumps})
+# necessary for named indexes to work in migrations
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+db = SQLAlchemy(
+    engine_options={'json_serializer': dumps},
+    metadata=MetaData(naming_convention=naming_convention),
+)
 
 
 def as_dict(instance):
